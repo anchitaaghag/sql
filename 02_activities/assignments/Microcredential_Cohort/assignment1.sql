73fe1aa1db9a8ec7b68f3f4240e056f248e27fb9
anchitaa <anchitaaghag@gmail.com>
2026-05-02 22:06:43 -0400

    Add query 1-10 answers

--- 02_activities/assignments/Microcredential_Cohort/assignment1.sql
+++ 02_activities/assignments/Microcredential_Cohort/assignment1.sql
@@ -7,8 +7,8 @@
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
 
-
-
+SELECT *
+FROM customer;
 
 --END QUERY
 
@@ -17,8 +17,10 @@
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
 
-
-
+SELECT *
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 --END QUERY
 
@@ -28,8 +30,10 @@ sorted by customer_last_name, then customer_first_ name. */
 Limit to 25 rows of output. */
 --QUERY 3
 
-
-
+SELECT *
+FROM customer_purchases
+WHERE product_id IN (4, 9)
+LIMIT 25;
 
 --END QUERY
 
@@ -43,8 +47,11 @@ Limit to 25 rows of output.
 */
 --QUERY 4
 
-
-
+SELECT *,
+		quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10
+LIMIT 25;
 
 --END QUERY
 
@@ -56,8 +63,14 @@ columns and add a column called prod_qty_type_condensed that displays the word �
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 5
 
-
-
+SELECT product_id,
+		product_name,
+		
+		CASE WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+		END AS  prod_qty_type_condensed
+		
+FROM product;
 
 --END QUERY
 
@@ -67,8 +80,18 @@ add a column to the previous query called pepper_flag that outputs a 1 if the pr
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 6
 
-
-
+SELECT product_id,
+		product_name,
+		
+		CASE WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+		END AS prod_qty_type_condensed,
+		
+		CASE WHEN product_name LIKE '%pepper%' THEN 1
+		ELSE 0
+		END AS pepper_flag
+		
+FROM product;
 
 --END QUERY
 
@@ -79,8 +102,12 @@ vendor_id field they both have in common, and sorts the result by market_date, t
 Limit to 24 rows of output. */
 --QUERY 7
 
-
-
+SELECT *
+FROM vendor
+INNER JOIN vendor_booth_assignments 
+ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER BY market_date, vendor_name
+LIMIT 24;
 
 --END QUERY
 
@@ -93,8 +120,10 @@ Limit to 24 rows of output. */
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 --QUERY 8
 
-
-
+SELECT vendor_id,
+		COUNT(vendor_id) AS times_booth_rented
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 --END QUERY
 
@@ -106,8 +135,15 @@ of customers for them to give stickers to, sorted by last name, then first name.
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 --QUERY 9
 
-
-
+SELECT c.customer_last_name,
+		c.customer_first_name,
+		ROUND(SUM(cp.quantity * cp.cost_to_customer_per_qty),2) AS total_spend
+FROM customer AS c
+INNER JOIN customer_purchases AS cp 
+ON c.customer_id = cp.customer_id
+GROUP BY c.customer_id
+HAVING total_spend > 2000
+ORDER BY c.customer_last_name, c.customer_first_name;
 
 --END QUERY
 
@@ -125,7 +161,28 @@ VALUES(col1,col2,col3,col4,col5)
 */
 --QUERY 10
 
+-- adding this here to be deleted
+DROP TABLE IF EXISTS temp.new_vendor;
 
+-- actual code
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
+
+INSERT INTO temp.new_vendor(
+		vendor_id,
+		vendor_name,
+		vendor_type,
+		vendor_owner_first_name,
+		vendor_owner_last_name
+		)
+VALUES(
+		10,
+		"Thomass Superfood Store",
+		"Fresh Focused",
+		'Thomas',
+		'Rosenthal'
+		);
 
 
 --END QUERY
